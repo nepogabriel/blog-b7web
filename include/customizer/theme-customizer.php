5dbef53dbbd07dd7e6b1b60506250a4f3b5b931a
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2020-01-15 23:54:41 -0200

    Criado área de depoimentos aleatórios.

--- include/customizer/theme-customizer.php
+++ include/customizer/theme-customizer.php
@@ -1,8 +1,10 @@
 <?php
 require get_template_directory().'/include/customizer/social.php';
+require get_template_directory().'/include/customizer/depositions.php';
 
 function gb_customize_register( $wp_customize ) {
 
     gb_social_customizer( $wp_customize );
+    gb_depositions_customizer( $wp_customize );
 
 }
